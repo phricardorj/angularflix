7dc8893e642f8af088989c5e779883a8818249da
Pedro Ricardo <70300680+phricardorj@users.noreply.github.com>
2021-11-23 16:43:04 -0300

    Update (*)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { FooterComponent } from './footer/footer.component';
-import { HeaderComponent } from './header/header.component';
-import { PromoLeftComponent } from './promo-left/promo-left.component';
-import { PromoRightComponent } from './promo-right/promo-right.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { HeaderComponent } from './components/header/header.component';
+import { PromoLeftComponent } from './components/promo-left/promo-left.component';
+import { PromoRightComponent } from './components/promo-right/promo-right.component';
 
 @NgModule({
   declarations: [
