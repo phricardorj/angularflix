9821408e53cc528f9babefc82390de48941a0010
josebiaggio <josebiaggio19@gmail.com>
2021-11-22 21:54:15 -0300

    Addition of the promotionGuide component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PromotionalGuideComponent } from './promotional-guide/promotional-guide.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PromotionalGuideComponent
   ],
   imports: [
     BrowserModule,
