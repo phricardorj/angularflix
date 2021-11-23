50499e9058093d685776c4cfd072fd375dff364a
Pedro Ricardo <70300680+phricardorj@users.noreply.github.com>
2021-11-23 16:32:37 -0300

    Update Tela Inicial

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FooterComponent } from './footer/footer.component';
+import { HeaderComponent } from './header/header.component';
+import { PromoLeftComponent } from './promo-left/promo-left.component';
+import { PromoRightComponent } from './promo-right/promo-right.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FooterComponent,
+    HeaderComponent,
+    PromoLeftComponent,
+    PromoRightComponent
   ],
   imports: [
     BrowserModule,
