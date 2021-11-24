14bb6ca8d071040689308fcffdf275ce8886cad6
Pedro Ricardo <70300680+phricardorj@users.noreply.github.com>
2021-11-23 23:36:33 -0300

    Update (*)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,16 @@ import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { LoginComponent } from './pages/login/login.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { HeaderComponent } from './components/header/header.component';
 import { PromoLeftComponent } from './components/promo-left/promo-left.component';
 import { PromoRightComponent } from './components/promo-right/promo-right.component';
+import { LoginFormComponent } from './components/login-form/login-form.component';
 import { HomePageComponent } from './pages/home-page/home-page.component';
 import { SelectProfileComponent } from './pages/select-profile/select-profile.component';
 import { LoggedComponent } from './pages/logged/logged.component';
-import { LoginFormComponent } from './login-form/login-form.component';
+import { LoginComponent } from './pages/login/login.component';
+
 
 @NgModule({
   declarations: [
