488395ef6b6dd61e227a225c4d77be20f8648c74
Thiago Assi <thiago-assi@outlook.com>
2021-11-23 21:48:40 -0300

    changed directory structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,19 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { LoginModule } from './login/login.module';
 import { PromotionalGuideComponent } from './promotional-guide/promotional-guide.component';
+import { LoginFormComponent } from './components/login-form/login-form.component';
+import { LoginComponent } from './pages/login/login.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
-  declarations: [AppComponent, PromotionalGuideComponent],
-  imports: [BrowserModule, AppRoutingModule, LoginModule],
+  declarations: [
+    AppComponent,
+    PromotionalGuideComponent,
+    LoginComponent,
+    LoginFormComponent,
+  ],
+  imports: [BrowserModule, AppRoutingModule, ReactiveFormsModule],
   providers: [],
   bootstrap: [AppComponent],
 })
