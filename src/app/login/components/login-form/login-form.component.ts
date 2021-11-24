d114f02bba48cf557423a3fa761fab4f76a31daf
Thiago Assi <thiago-assi@outlook.com>
2021-11-23 21:35:00 -0300

    login form validation

--- src/app/login/components/login-form/login-form.component.ts
+++ src/app/login/components/login-form/login-form.component.ts
@@ -1,15 +1,29 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login-form',
   templateUrl: './login-form.component.html',
-  styleUrls: ['./login-form.component.css']
+  styleUrls: ['./login-form.component.css'],
 })
 export class LoginFormComponent implements OnInit {
+  loginForm = new FormGroup({
+    user: new FormControl('', Validators.required),
+    password: new FormControl('', [
+      Validators.required,
+      Validators.minLength(4),
+      Validators.maxLength(60),
+    ]),
+  });
 
-  constructor() { }
+  showMore = false;
 
-  ngOnInit(): void {
+  onSubmit() {
+    // TODO: Use EventEmitter with form value
+    console.warn(this.loginForm.value);
   }
 
+  constructor() {}
+
+  ngOnInit(): void {}
 }
--- src/app/login/login.module.ts
+++ src/app/login/login.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LoginFormComponent } from './components/login-form/login-form.component';
 import { LoginScreenComponent } from './components/login-screen/login-screen.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [LoginFormComponent, LoginScreenComponent],
-  imports: [CommonModule],
+  imports: [CommonModule, ReactiveFormsModule],
   exports: [LoginScreenComponent],
 })
 export class LoginModule {}
