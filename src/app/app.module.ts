9c4e3795f23753b4228b344d8f4a305c70d183a7
Thiago Assi <thiago-assi@outlook.com>
2021-11-23 22:14:07 -0300

    fixed app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,16 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PromotionalGuideComponent } from './promotional-guide/promotional-guide.component';
 import { LoginFormComponent } from './components/login-form/login-form.component';
 import { LoginComponent } from './pages/login/login.component';
-import { ReactiveFormsModule } from '@angular/forms';
 import { FooterComponent } from './components/footer/footer.component';
 import { HeaderComponent } from './components/header/header.component';
 import { PromoLeftComponent } from './components/promo-left/promo-left.component';
 import { PromoRightComponent } from './components/promo-right/promo-right.component';
-import { LoginComponent } from './pages/login/login.component';
 import { HomePageComponent } from './pages/home-page/home-page.component';
 import { SelectProfileComponent } from './pages/select-profile/select-profile.component';
 import { LoggedComponent } from './pages/logged/logged.component';
@@ -26,14 +25,9 @@ import { LoggedComponent } from './pages/logged/logged.component';
     HeaderComponent,
     PromoLeftComponent,
     PromoRightComponent,
-    LoginComponent,
     HomePageComponent,
     SelectProfileComponent,
-    LoggedComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
+    LoggedComponent,
   ],
   imports: [BrowserModule, AppRoutingModule, ReactiveFormsModule],
   providers: [],
