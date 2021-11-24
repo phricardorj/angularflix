4a2a673623fde017e23d9de3d85b4b67e7d5b487
Pedro Ricardo <70300680+phricardorj@users.noreply.github.com>
2021-11-23 21:23:56 -0300

    Add Routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomePageComponent } from './pages/home-page/home-page.component';
+import { LoginComponent } from './pages/login/login.component';
+import { SelectProfileComponent } from './pages/select-profile/select-profile.component';
+import { LoggedComponent } from './pages/logged/logged.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomePageComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'profiles', component: SelectProfileComponent },
+  { path: 'logged', component: LoggedComponent }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [ RouterModule ],
+  imports: [ RouterModule.forRoot(routes) ]
 })
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,10 @@ import { FooterComponent } from './components/footer/footer.component';
 import { HeaderComponent } from './components/header/header.component';
 import { PromoLeftComponent } from './components/promo-left/promo-left.component';
 import { PromoRightComponent } from './components/promo-right/promo-right.component';
+import { LoginComponent } from './pages/login/login.component';
+import { HomePageComponent } from './pages/home-page/home-page.component';
+import { SelectProfileComponent } from './pages/select-profile/select-profile.component';
+import { LoggedComponent } from './pages/logged/logged.component';
 
 @NgModule({
   declarations: [
@@ -14,7 +18,11 @@ import { PromoRightComponent } from './components/promo-right/promo-right.compon
     FooterComponent,
     HeaderComponent,
     PromoLeftComponent,
-    PromoRightComponent
+    PromoRightComponent,
+    LoginComponent,
+    HomePageComponent,
+    SelectProfileComponent,
+    LoggedComponent
   ],
   imports: [
     BrowserModule,
