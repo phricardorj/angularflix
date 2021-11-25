4baeeb377af314bb2fa8d08d67bec5deefe3c2a3
Thiago Assi <thiago-assi@outlook.com>
2021-11-25 16:26:16 -0300

    code refactoring

--- src/app/components/login-form/login-form.component.ts
+++ src/app/components/login-form/login-form.component.ts
@@ -1,14 +1,14 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
-import { LoginFormService } from '../login-form/login-form.service'
+import { Router } from '@angular/router';
+import { LoginFormService } from '../login-form/login-form.service';
 
 @Component({
   selector: 'app-login-form',
   templateUrl: './login-form.component.html',
   styleUrls: ['./login-form.component.css'],
 })
-
 export class LoginFormComponent implements OnInit {
   loginForm = new FormGroup({
     user: new FormControl('', Validators.required),
@@ -19,56 +19,60 @@ export class LoginFormComponent implements OnInit {
     ]),
   });
 
-  constructor(private loginFormService: LoginFormService) {}
+  constructor(
+    private loginFormService: LoginFormService,
+    private router: Router
+  ) {}
+
+  ngOnInit() {}
 
   showMore = false;
   toggleShowMore() {
     this.showMore = !this.showMore;
   }
 
-  url: string = 'https://private-3923c4-santandercoders809.apiary-mock.com/login';
-  loginData: any = {};
+  url: string =
+    'https://private-3923c4-santandercoders809.apiary-mock.com/login';
+  loginData: any = { users: 'squad08@letscode.com', password: 'squad08' };
   user: string = '';
   password: string = '';
   userIsValid: string = '';
 
   postLogin(user: string, password: string) {
-    return new Promise(resolve => {
-      this.loginFormService.postLogin(this.url, user, password)
-        .subscribe(data => resolve(data))
-    })
+    return new Promise((resolve) => {
+      this.loginFormService
+        .postLogin(this.url, user, password)
+        .subscribe((data) => resolve(data));
+    });
   }
 
   async onSubmit() {
-    const user = this.loginForm.get('user')?.value
-    const password = this.loginForm.get('password')?.value
+    const user = this.loginForm.get('user')?.value;
+    const password = this.loginForm.get('password')?.value;
 
-    this.loginFormService.postLogin(this.url, user, password)
-      .subscribe((data) => this.loginData = data);
+    this.loginFormService
+      .postLogin(this.url, user, password)
+      .subscribe((data) => (this.loginData = data));
 
     this.loginData = await this.postLogin(user, password);
 
     const token = JSON.parse(this.loginData).token;
     localStorage.setItem('token', token);
 
-    this.checkIfUserIsValid(user, password)
-
-    // TODO: Use EventEmitter with form value
-    console.warn(this.loginForm.value);
+    this.checkIfUserIsValid(user, password);
   }
 
   checkIfUserIsValid(user: string, password: string) {
-    const users = JSON.parse(this.loginData).users
-    const id = parseInt(user.replace(/[^0-9]/g,''));
+    const users = JSON.parse(this.loginData).users;
+    const id = parseInt(user.replace(/[^0-9]/g, ''));
 
     users.forEach((user: any) => {
-      if(user.id === id) this.userIsValid = 'Usuário válido'
-        else this.userIsValid = 'Usuário inválido'
+      if (user.id === id) this.userIsValid = 'Usuário válido';
+      else this.userIsValid = 'Usuário inválido';
     });
 
-    console.log(this.userIsValid)
-  }
-
-  ngOnInit() {
+    console.log(this.userIsValid);
+    if (this.userIsValid === 'Usuário válido')
+      this.router.navigate(['/catalog']);
   }
 }
