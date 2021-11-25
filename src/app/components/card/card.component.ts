f2f462b6aba58b77fceed7c865696fa7dc5c957e
LucasSuplino <lucas.suplino@gmail.com>
2021-11-25 18:34:56 -0300

    Adcionando getFunction

--- src/app/components/card/card.component.ts
+++ src/app/components/card/card.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ElementRef } from '@angular/core';
+import { LoginFormService } from 'src/app/components/login-form/login-form.service';
 
 @Component({
   selector: 'app-card',
@@ -6,10 +7,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./card.component.css']
 })
 export class CardComponent implements OnInit {
+  // image: string;
+  serieId: string;
+  url: string = 'https://private-3923c4-santandercoders809.apiary-mock.com/series/';
+  serie: any = {};
+  image: string = '';
+  property:number = 0;
 
-  constructor() { }
+  constructor(private loginFormService: LoginFormService, private elementRef: ElementRef) {
 
-  ngOnInit(): void {
+    this.serieId = this.elementRef.nativeElement.getAttribute('serieId');
   }
+
+  ngOnInit() {
+    this.loginFormService.getInfo(`${this.url}${this.serieId}`).subscribe((x) => {
+      this.serie = x;
+      this.image = this.serie['cardImage'];
+    });
+  }
+
+
+  increment_property() {
+        return this.property++;
+  }
+   
 
 }
--- src/app/pages/catalog/catalog.component.ts
+++ src/app/pages/catalog/catalog.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { LoginFormService } from 'src/app/components/login-form/login-form.service';
 
 @Component({
   selector: 'app-catalog',
@@ -7,9 +8,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CatalogComponent implements OnInit {
 
-  constructor() { }
+  constructor(private loginFormService: LoginFormService) { }
+  loading = false;
+  cards: any = {};
+  popular: any = {};
+  keepWatching: any = {};
+  url: string = 'https://private-3923c4-santandercoders809.apiary-mock.com/';
 
-  ngOnInit(): void {
+  ngOnInit() {
+    this.loading = true;
+    this.loginFormService.getInfo(`${this.url}${"users/1"}`).subscribe((x) => {
+      this.cards = x;
+      this.loading = false;
+      this.popular = this.cards['popular'];
+      this.keepWatching = this.cards['keepWatching'];
+      console.log(this.popular);
+      console.log(this.keepWatching);
+    });
   }
 
 }
