3c608db6e5eafb960cf42a5f58c32384454b89a7
Thiago Assi <thiago-assi@outlook.com>
2021-11-26 12:08:50 -0300

    fixed catalog component

--- src/app/pages/catalog/catalog.component.ts
+++ src/app/pages/catalog/catalog.component.ts
@@ -1,15 +1,29 @@
 import { Component, OnInit } from '@angular/core';
+import { LoginFormService } from 'src/app/components/login-form/login-form.service';
 
 @Component({
   selector: 'app-catalog',
   templateUrl: './catalog.component.html',
-  styleUrls: ['./catalog.component.css']
+  styleUrls: ['./catalog.component.css'],
 })
 export class CatalogComponent implements OnInit {
+  constructor(private loginFormService: LoginFormService) {}
+  loading = false;
+  cards: any = {};
+  popular: any = {};
+  popular2: any = ['5', '6', '7'];
+  keepWatching: any = {};
+  url: string = 'https://private-3923c4-santandercoders809.apiary-mock.com/';
 
-  constructor() { }
-
-  ngOnInit(): void {
+  ngOnInit() {
+    this.loading = true;
+    this.loginFormService.getInfo(`${this.url}${'users/1'}`).subscribe((x) => {
+      this.cards = x;
+      this.loading = false;
+      this.popular = this.cards['popular'];
+      this.keepWatching = this.cards['keepWatching'];
+      console.log(this.popular);
+      console.log(this.keepWatching);
+    });
   }
-
 }
