3ce001a540311112c2f872935855acb2988eef05
Thiago Assi <thiago-assi@outlook.com>
2021-11-26 10:58:38 -0300

    updated select-profile page

--- src/app/pages/select-profile/select-profile.component.ts
+++ src/app/pages/select-profile/select-profile.component.ts
@@ -1,15 +1,38 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+interface userData {
+  token?: string;
+  users?: [
+    {
+      id: number;
+      name: string;
+      avatarUrl: string;
+    }
+  ];
+}
 
 @Component({
   selector: 'app-select-profile',
   templateUrl: './select-profile.component.html',
-  styleUrls: ['./select-profile.component.css']
+  styleUrls: ['./select-profile.component.css'],
 })
 export class SelectProfileComponent implements OnInit {
+  constructor(private router: Router) {}
 
-  constructor() { }
+  userData: userData = {};
 
   ngOnInit(): void {
+    const users = localStorage.getItem('users');
+    if (!users) {
+      this.router.navigate(['/login']);
+      return;
+    }
+
+    this.userData = JSON.parse(users);
   }
 
+  selectUser(id: number): void {
+    localStorage.setItem('userId', id.toString());
+  }
 }
