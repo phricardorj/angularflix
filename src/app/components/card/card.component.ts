bf0179d6872c0f173ed2afc2ba9d68ea4263674c
Pedro Ricardo <70300680+phricardorj@users.noreply.github.com>
2021-11-26 02:08:29 -0300

    Fix Bugs

--- src/app/components/card/card.component.ts
+++ src/app/components/card/card.component.ts
@@ -1,5 +1,4 @@
-import { Component, Input, OnInit} from '@angular/core';
-import { LoginFormService } from 'src/app/components/login-form/login-form.service';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-card',
@@ -7,30 +6,10 @@ import { LoginFormService } from 'src/app/components/login-form/login-form.servi
   styleUrls: ['./card.component.css']
 })
 export class CardComponent implements OnInit {
-  // image: string;
-  @Input() text: string ='';
-  // serieId: string;
-  url: string = 'https://private-3923c4-santandercoders809.apiary-mock.com/series/';
-  serie: any = {};
-  image: string = '';
-  property:number = 0;
 
-  constructor(private loginFormService: LoginFormService) {
-    // this.serieId = this.elementRef.nativeElement.getAttribute('serieId');
-    // this.serieId = "1"
+  constructor() { }
+
+  ngOnInit(): void {
   }
-
-  ngOnInit() {
-    this.loginFormService.getInfo(`${this.url}${this.text}`).subscribe((x) => {
-      this.serie = x;
-      this.image = this.serie['cardImage'];
-    });
-  }
-
-
-  increment_property() {
-        return this.property++;
-  }
-   
 
 }
